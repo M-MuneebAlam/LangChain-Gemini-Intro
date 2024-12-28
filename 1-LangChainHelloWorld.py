12fa356b7a94fd6778863a05bda2e99d3d86c760
M-MuneebAlam <mmuneeb.alam09@gmail.com>
2024-12-28 18:18:55 +0500

    Add Langchain's Memory feature

--- 1-LangChainHelloWorld.py
+++ 1-LangChainHelloWorld.py
@@ -2,7 +2,9 @@
 from dotenv import load_dotenv
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
 
 
 # Load variables from .env
@@ -18,17 +20,53 @@ llm = ChatGoogleGenerativeAI(
 
 
 # Define a prompt template
-prompt_template = PromptTemplate.from_template(
-    "You are a helpful assistant. Answer the following question: {question}"
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant. Answer all questions to the best of your ability.",
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{input}"),
+    ]
 )
 
 # Build the LangChain Pipeline
-chain = prompt_template | llm
+chain = prompt | llm
 
 
-# Define a user question
-response = chain.invoke({"question" : "Who won the ICC world cup in 1992? And who was the captain of winning team?"})
+
+history = ChatMessageHistory()
+
+chain_with_message_history = RunnableWithMessageHistory(
+    chain,
+    lambda session_id: history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+)
+
+response1 = chain_with_message_history.invoke(
+    {"input": "Who won the ICC world cup in 1992?"},
+    {"configurable": {"session_id": "unused"}},
+)
+
+response2 = chain_with_message_history.invoke(
+    {"input": "Who was the captain of winning team?"},
+    {"configurable": {"session_id": "unused"}},
+)
+
 
 
 # Display the result
-print("Response:", response.content)
+print("First Response:", response1.content)
+print("\n\nSecond Response:", response2.content)
+
+
+
+# Access the chat history messages:
+chat_history_messages = history.messages
+
+# Print the chat history messages:
+for message in chat_history_messages:
+    print(f"{message.type}: {message.content}")
+
